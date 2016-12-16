9e26a49777e9ed5fb9164cdf20a08009d96384cc
Josh Petitt <joshpetitt@jpmec.com>
2016-12-15 21:57:39 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+LOCLASSPY
+----
+
+A demonstration of functional programming in Python.
+
+Python is not a functional programming language, but who cares?
+
+Let's see what we can do!
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='loclasspy',
+    version='0.0.0',
+    description='Demonstration of functional programming with Python',
+    url='https://github.com/jpmec/loclasspy',
+    author='Joshua Petitt',
+    license='MIT',
+    packages=['loclasspy'],
+    zip_safe=False
+)
